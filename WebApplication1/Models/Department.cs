41c1e909d72c257690236de67669efe92db947da
Natan A. Costa <natanarcosta@gmail.com>
2021-04-03 12:56:17 -0300

    API for Department Screen : GET

--- WebApplication1/Models/Department.cs
+++ WebApplication1/Models/Department.cs
@@ -7,5 +7,9 @@ namespace WebApplication1.Models
 {
     public class Department
     {
+        public int DepartmentId { get; set; }
+        public string DepartmentName { get; set; }
+
+
     }
 }
